374904e2d8aa9c2c9a15f3f3ce69f1770c6157af
Christian Lang <lang.chr86@gmail.com>
2017-02-19 13:16:26 +0100

    + Add git user info.

--- lang-mainserver/prepare-apps.bash
+++ lang-mainserver/prepare-apps.bash
@@ -7,3 +7,5 @@ apt-get install tmux tree
 apt-get install openssh-client openssh-server
 apt-get install samba
 
+git config --global user.name "Christian Lang"
+git config --global user.email "lang.chr86@gmail.com"
