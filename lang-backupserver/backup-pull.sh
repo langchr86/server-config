923cacb74d91899feffde18479a42f2c7077589c
Christian Lang <lang.chr86@gmail.com>
2017-02-22 19:52:16 +0100

    * Adapt to new IP and general adaption for new server installation.

--- lang-backupserver/backup-pull.sh
+++ lang-backupserver/backup-pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # main-share backup pull
-rsync -a --delete --stats clang@192.168.0.11:/mnt/subvolumes/share-main/ /mnt/subvolumes/share-main
+rsync -a --delete --stats clang@192.168.0.6:/mnt/subvolumes/share-main/ /mnt/subvolumes/share-main
