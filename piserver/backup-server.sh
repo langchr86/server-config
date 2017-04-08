ef6b6ed0b18bd0133d6d195d74e68e0749415e40
Christian Lang <lang.chr86@gmail.com>
2017-04-08 11:13:22 +0200

    ! Main NAS needs also to be started.

--- piserver/backup-server.sh
+++ piserver/backup-server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-FORCE_ON="/share/lang-nas08/force_on"
+NAS08="/share/lang-nas08/force_on"
+NAS16="/share/lang-nas16/force_on"
 
-# startup backup server
-touch ${FORCE_ON}
+
+
+# startup main and backup server for backup task
+touch ${NAS16}
+cleep 1m
+touch ${NAS08}
 sleep 15m
-rm -f ${FORCE_ON}
+rm -f ${NAS08} ${NAS16}
