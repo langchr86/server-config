f1db0a603f7272ecd93b61e633e34cadd59ffcdf
Christian Lang <lang.chr86@gmail.com>
2018-09-18 19:49:29 +0200

    * prepare script has auto yes option for apt-get

--- prepare-apps.bash
+++ prepare-apps.bash
@@ -2,15 +2,15 @@
 
 apt-get update
 
-apt-get install btrfs-tools
-apt-get install lvm2
-apt-get install git-core git-gui gitk
-apt-get install tmux tree
-apt-get install openssh-client openssh-server
-apt-get install samba
-apt-get install docker.io
-apt-get install htop iotop
-apt-get install smartmontools
+apt-get install -y btrfs-tools
+apt-get install -y lvm2
+apt-get install -y git-core git-gui gitk
+apt-get install -y tmux tree
+apt-get install -y openssh-client openssh-server
+apt-get install -y samba
+apt-get install -y docker.io
+apt-get install -y htop iotop
+apt-get install -y smartmontools
 
 git config --global user.name "Christian Lang"
 git config --global user.email "lang.chr86@gmail.com"
