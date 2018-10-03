8757e51b0dff6b8c7e3086af5bf5737386c5f4e8
Christian Lang <lang.chr86@gmail.com>
2018-10-03 19:05:11 +0200

    ! use all files in folders for backup-pull and not whole folders
    this does not work for symlinks

--- mainserver/backup-pull.sh
+++ mainserver/backup-pull.sh
@@ -11,10 +11,10 @@ DEST="/mnt/subvolumes/share-main/Backups/lang-piserver"
 # piserver backup pull
 mkdir -p ${DEST}
 
-rsync -a --delete --stats ${ADDR}:/git ${DEST}/lang-piserver_git/
+rsync -a --delete --stats ${ADDR}:/git/ ${DEST}/lang-piserver_git/
 
-rsync -a --delete --stats ${ADDR}:/share/data ${DEST}/lang-piserver_share/
-rsync -a --delete --stats ${ADDR}:/share/fileshare ${DEST}/lang-piserver_share/
-rsync -a --delete --stats ${ADDR}:/share/gallery ${DEST}/lang-piserver_share/
-rsync -a --delete --stats ${ADDR}:/share/projects ${DEST}/lang-piserver_share/
-rsync -a --delete --stats ${ADDR}:/share/temp ${DEST}/lang-piserver_share/
+rsync -a --delete --stats ${ADDR}:/share/data/ ${DEST}/lang-piserver_share/data/
+rsync -a --delete --stats ${ADDR}:/share/fileshare/ ${DEST}/lang-piserver_share/fileshare/
+rsync -a --delete --stats ${ADDR}:/share/gallery/ ${DEST}/lang-piserver_share/gallery/
+rsync -a --delete --stats ${ADDR}:/share/projects/ ${DEST}/lang-piserver_share/projects/
+rsync -a --delete --stats ${ADDR}:/share/temp/ ${DEST}/lang-piserver_share/temp/
