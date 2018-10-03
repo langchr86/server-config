6e863434773d02c921757db91af8ef18f3c5d177
Christian Lang <lang.chr86@gmail.com>
2018-10-03 18:45:47 +0200

    * make prepare script use sudo and remove special apps

--- prepare-apps.bash
+++ prepare-apps.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y --no-install-recommends \
-  btrfs-tools lvm2 smartmontools ledmon \
+sudo apt-get install -y --no-install-recommends \
+  smartmontools rsync \
   git-core git-gui gitk \
   tmux tree htop iotop sysstat dstat \
-  openssh-client openssh-server \
-  docker.io samba
+  openssh-client openssh-server
 
 git config --global user.name "Christian Lang"
 git config --global user.email "lang.chr86@gmail.com"
@@ -17,4 +16,4 @@ cp .tmux.conf ~
 cp tmux-helper.bash ~
 cp git-helper.bash ~
 
-cp ./btrfs-snap/btrfs-snap /usr/bin
+sudo cp ./btrfs-snap/btrfs-snap /usr/bin
