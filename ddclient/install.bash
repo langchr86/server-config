394df813c83517da2397fa2649be626b3216f123
Christian Lang <lang.chr86@gmail.com>
2018-10-18 20:33:37 +0200

    + add complete install steps into ddclient script

--- ddclient/install.bash
+++ ddclient/install.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+SD_PATH="/etc/systemd/system"
+
+cp -f ddclient.service ${SD_PATH}
+cp -f ddclient.timer ${SD_PATH}
+
+systemctl daemon-reload
+
+
 cp -f ddclient /usr/sbin/
 mkdir -p /etc/ddclient/
 cp -f ddclient.conf /etc/ddclient/
 chmod 600 /etc/ddclient/ddclient.conf
+
+
+systemctl enable ddclient.timer
+systemctl start ddclient.timer
