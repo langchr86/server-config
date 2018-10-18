f8d18a799dcb7ca92fd3a586c053f7dbb88a96b7
Christian Lang <lang.chr86@gmail.com>
2018-10-18 20:45:22 +0200

    ! ensure cache folder is created for ddclient

--- ddclient/install.bash
+++ ddclient/install.bash
@@ -12,6 +12,7 @@ cp -f ddclient /usr/sbin/
 mkdir -p /etc/ddclient/
 cp -f ddclient.conf /etc/ddclient/
 chmod 600 /etc/ddclient/ddclient.conf
+mkdir -p /var/cache/ddclient/
 
 
 systemctl enable ddclient.timer
