87d00dc6ded49029bd1d42243fba5b9913f30186
Christian Lang <lang.chr86@gmail.com>
2018-11-06 18:54:50 +0100

    + add backup for resilio folder and change hostname to miniserver

--- mainserver/backup-pull.sh
+++ mainserver/backup-pull.sh
@@ -6,15 +6,17 @@ sleep 30
 
 
 ADDR="christian@192.168.0.8"
-DEST="/mnt/subvolumes/share-main/Backups/lang-piserver"
+DEST="/mnt/subvolumes/share-main/Backups/lang-miniserver"
 
 # piserver backup pull
 mkdir -p ${DEST}
 
-rsync -a --delete --stats ${ADDR}:/git/ ${DEST}/lang-piserver_git/
+rsync -a --delete --stats ${ADDR}:/git/ ${DEST}/lang-miniserver_git/
 
-rsync -a --delete --stats ${ADDR}:/share/data/ ${DEST}/lang-piserver_share/data/
-rsync -a --delete --stats ${ADDR}:/share/fileshare/ ${DEST}/lang-piserver_share/fileshare/
-rsync -a --delete --stats ${ADDR}:/share/gallery/ ${DEST}/lang-piserver_share/gallery/
-rsync -a --delete --stats ${ADDR}:/share/projects/ ${DEST}/lang-piserver_share/projects/
-rsync -a --delete --stats ${ADDR}:/share/temp/ ${DEST}/lang-piserver_share/temp/
+rsync -a --delete --stats ${ADDR}:/resilio/ ${DEST}/lang-miniserver_resilio/
+
+rsync -a --delete --stats ${ADDR}:/share/data/ ${DEST}/lang-miniserver_share/data/
+rsync -a --delete --stats ${ADDR}:/share/fileshare/ ${DEST}/lang-miniserver_share/fileshare/
+rsync -a --delete --stats ${ADDR}:/share/gallery/ ${DEST}/lang-miniserver_share/gallery/
+rsync -a --delete --stats ${ADDR}:/share/projects/ ${DEST}/lang-miniserver_share/projects/
+rsync -a --delete --stats ${ADDR}:/share/temp/ ${DEST}/lang-miniserver_share/temp/
