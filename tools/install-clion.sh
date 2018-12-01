be8b58143df6181a05e8be7edfad5200eef4e972
Christian Lang <lang.chr86@gmail.com>
2018-12-01 15:36:28 +0100

    + add version variable and install notification in install-clion.sh

--- tools/install-clion.sh
+++ tools/install-clion.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+VERSION="2018.3"
+
 # install CLion
 cd ~
+
+echo "installing CLion ${VERSION}"
 if [ ! -d "clion" ]; then
-  wget -q https://download.jetbrains.com/cpp/CLion-2018.3.tar.gz
+  wget -q https://download.jetbrains.com/cpp/CLion-${VERSION}.tar.gz
   mkdir clion
   tar -zxf CLion-*.tar.gz -C clion
+  rm CLion-*.tar.gz
 
-  echo "alias clion=\"~/clion/clion-2018.3/bin/clion.sh\"" >> ~/.bash_aliases
+  echo "alias clion=\"~/clion/clion-${VERSION}/bin/clion.sh\"" >> ~/.bash_aliases
 fi
