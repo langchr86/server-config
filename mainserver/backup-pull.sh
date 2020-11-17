ad0759ca396dd5206a95f5b8c0be7382c65f0f8c
Christian Lang <lang.chr86@gmail.com>
2020-11-17 21:00:27 +0100

    + add config folder to backup-pull.sh

--- mainserver/backup-pull.sh
+++ mainserver/backup-pull.sh
@@ -13,6 +13,7 @@ mkdir -p ${DEST}/lang-miniserver_git/
 rsync -a --delete --stats ${ADDR}:/git/ ${DEST}/lang-miniserver_git/
 
 mkdir -p ${DEST}/lang-miniserver_share/
+rsync -a --delete --stats ${ADDR}:/share/config/ ${DEST}/lang-miniserver_share/config/
 rsync -a --delete --stats ${ADDR}:/share/data/ ${DEST}/lang-miniserver_share/data/
 rsync -a --delete --stats ${ADDR}:/share/fileshare/ ${DEST}/lang-miniserver_share/fileshare/
 rsync -a --delete --stats ${ADDR}:/share/gallery/ ${DEST}/lang-miniserver_share/gallery/
