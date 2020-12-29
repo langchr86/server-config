cb9920586049c8ce39ed27a16df65a37a31f7c77
Christian Lang <lang.chr86@gmail.com>
2020-12-29 15:25:18 +0100

    * add basic setup steps in ansible

--- setup-buildtools.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install -y  \
-  build-essential gcc \
-  cmake automake autoconf pkg-config \
-  valgrind gcovr
--- setup-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install -y \
-  docker.io
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
