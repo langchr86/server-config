676001f23e26cc438c5eb93ef0a6843e09bdaece
Christian Lang <lang.chr86@gmail.com>
2021-01-01 18:10:19 +0100

    ! add username for playbook scripts

--- mini.sh
+++ mini.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 ansible-galaxy install -r ${SCRIPT_DIR}/ansible/requirements.yml
 
 ansible-playbook \
-  --inventory 192.168.0.8, \
+  --inventory clang@192.168.0.8, \
   ${SCRIPT_DIR}/ansible/playbook_mini.yml
