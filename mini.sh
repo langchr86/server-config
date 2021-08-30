4fce2d85a6e793da68464ff6fedc1a93038f2155
Christian Lang <lang.chr86@gmail.com>
2021-08-30 20:02:01 +0200

    * change IPs for real ansible launcher scripts

--- mini.sh
+++ mini.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 ansible-galaxy install -r ${SCRIPT_DIR}/ansible/requirements.yml
 
 ansible-playbook \
-  --inventory clang@192.168.0.8, \
+  --inventory clang@192.168.0.9, \
   --vault-id infra@${SCRIPT_DIR}/infra_pw \
+  --extra-vars 'ansible_python_interpreter=/usr/bin/python3' \
   ${SCRIPT_DIR}/ansible/playbook_mini.yml
