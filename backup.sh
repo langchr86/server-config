83694992de238a24e468ffd2dbc8b91094691265
Christian Lang <lang.chr86@gmail.com>
2022-12-20 23:19:28 +0100

    * enforce updating ansible roles

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-ansible-galaxy install -r ${SCRIPT_DIR}/ansible/requirements.yml
+ansible-galaxy install -f -r ${SCRIPT_DIR}/ansible/requirements.yml
 
 ansible-playbook \
   --inventory clang@192.168.0.5, \
