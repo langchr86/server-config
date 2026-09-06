0c1cac8fabbfba4763677c8deca0b736eac94d4f
Christian Lang <lang.chr86@gmail.com>
2026-09-06 15:55:08 +0200

    WIP separate playbook

---
+++ wgeasy.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+ansible-galaxy install -f -r ${SCRIPT_DIR}/ansible/requirements.yml
+
+export ANSIBLE_CONFIG=${SCRIPT_DIR}/ansible/ansible.cfg
+
+ansible-playbook \
+  --inventory clang@192.168.0.9, \
+  --ask-become-pass \
+  --vault-id infra@${SCRIPT_DIR}/infra_pw \
+  --extra-vars 'ansible_python_interpreter=/usr/bin/python3' \
+  ${SCRIPT_DIR}/ansible/playbook_wgeasy.yml
